3ea524d826033d03ed700d99779371bd9ec6e612
c0sh <c0shcs@googlemail.com>
2022-08-24 10:21:20 +0200

    startseiten buttens und logo dummys intigriert

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,11 +1,9 @@
 package view;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
